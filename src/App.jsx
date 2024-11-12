c38e7eab029461682da49dba6f0f564cd6ab3fc8
WonYunSun <ereunosun113@gmail.com>
2024-11-12 19:20:13 +0900

    fix : app.js 수정(mockData 전달하는 부분 지움)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import GlobalStyles from "./Globalstyles.js";
 import Router from "./shared/Router";
-import MOCK_DATA from "../src/data/PokemonMockData.js";
+
 import { PokemonProvider } from "./context/PokemonContext.jsx";
 
 function App() {
@@ -9,7 +9,7 @@ function App() {
     <PokemonProvider>
       {/* 글로벌 폰트 적용 */}
       <GlobalStyles />
-      <Router mockData={MOCK_DATA} />
+      <Router />
     </PokemonProvider>
   );
 }
